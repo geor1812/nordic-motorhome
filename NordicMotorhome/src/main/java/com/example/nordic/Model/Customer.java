1cddc21f84d21e346ca5c350d0fa342e2bf438f1
George <george.ursachi7@gmail.com>
2020-05-26 14:15:27 +0200

    Technical error

---
+++ NordicMotorhome/src/main/java/com/example/nordic/Model/Customer.java
@@ -0,0 +1,113 @@
+package com.example.nordic.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    private int idCustomer;
+    private String firstName;
+    private String lastName;
+    private String phoneNo;
+    private String email;
+    private int idAddress;
+    private String address_details;
+    private String city;
+    private String country;
+    private String state;
+    private String zip;
+
+    public void createCustomer(){}
+
+    public int getIdCustomer() {
+        return idCustomer;
+    }
+
+    public void setIdCustomer(int idCustomer) {
+        this.idCustomer = idCustomer;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdAddress() {
+        return idAddress;
+    }
+
+    public void setIdAddress(int idAddress) {
+        this.idAddress = idAddress;
+    }
+
+    public String getAddress_details() {
+        return address_details;
+    }
+
+    public void setAddress_details(String address_details) {
+        this.address_details = address_details;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+
+}
--- NordicMotorhome/src/main/java/com/example/nordic/Service/VehicleService.java
+++ NordicMotorhome/src/main/java/com/example/nordic/Service/VehicleService.java
@@ -11,17 +11,26 @@ import java.util.List;
 public class VehicleService {
     @Autowired
     VehicleRepo vehicleRepo;
-
     private int workingID;
-    /***
+
+    /**
      * Gets the result set to from vehicleRepo
-     * @return
+     * @return query result set
      */
     public List<Vehicle> readAll() {
         return vehicleRepo.readAll();
     }
 
-    /***
+    /**
+     * Passes the search term to the vehicleRepo and then returns the result set
+     * @param search the search term
+     * @return query result set
+     */
+    public List<Vehicle> readSearch(String search) {
+        return vehicleRepo.readSearch(search);
+    }
+
+    /**
      * Passes the vehicle to be added to vehicleRepo
      * @param vehicle the vehicle to be added
      */
@@ -32,19 +41,39 @@ public class VehicleService {
         vehicleRepo.updateVehicle(id, vehicle);
     }
 
+    /**
+     * Sends the id and update vehicle information to vehicleRepo
+     * @param id the id of the vehicle to be updated
+     * @param vehicle a Vehicle object containing the updated information
+     */
+    public void updateVehicle(int id, Vehicle vehicle) {
+        vehicleRepo.updateVehicle(id, vehicle);
+    }
+
+    /**
+     * Returns a Vehicle object from the vehicleRepo
+     * @param id the id of the vehicle
+     **/
     public Vehicle findVehicleById(int id) {
         return vehicleRepo.findVehicleById(id);
     }
 
+    /**
+     * Sends the id of the vehicle that will be deleted to vehicleRepo
+     * @param idVehicle the id of the vehicle to be deleted
+     */
+    public void deleteVehicle(int idVehicle) {
+        vehicleRepo.deleteVehicle(idVehicle);
+    }
+
+    /*
+    Working ID Getters & Setters
+     */
     public int getWorkingID() {
         return workingID;
     }
 
     public void setWorkingID(int workingID) {
         this.workingID = workingID;
-    }
-
-    public boolean deleteVehicle(int idVehicle) {
-        return vehicleRepo.deleteVehicle(idVehicle);
     }
 }

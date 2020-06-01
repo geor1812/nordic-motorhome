01d13508724586188a4932d63c53219d194443b3
George <george.ursachi7@gmail.com>
2020-06-01 09:26:25 +0200

    Inheritance

--- NordicMotorhome/src/main/java/com/example/nordic/Model/Customer.java
+++ NordicMotorhome/src/main/java/com/example/nordic/Model/Customer.java
@@ -4,22 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-public class Customer {
+public class Customer extends Person {
 
     @Id
     private int idCustomer;
-    private String firstName;
-    private String lastName;
-    private String phoneNo;
-    private String email;
-    private int idAddress;
-    private String addressDetails;
-    private String city;
-    private String country;
-    private String state;
-    private String zip;
-
-    public void createCustomer(){}
 
     public int getIdCustomer() {
         return idCustomer;
@@ -28,86 +16,5 @@ public class Customer {
     public void setIdCustomer(int idCustomer) {
         this.idCustomer = idCustomer;
     }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNo() {
-        return phoneNo;
-    }
-
-    public void setPhoneNo(String phoneNo) {
-        this.phoneNo = phoneNo;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdAddress() {
-        return idAddress;
-    }
-
-    public void setIdAddress(int idAddress) {
-        this.idAddress = idAddress;
-    }
-
-    public String getAddressDetails() {
-        return addressDetails;
-    }
-
-    public void setAddressDetails(String address) {
-        this.addressDetails = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-
 
 }
---
+++ NordicMotorhome/src/main/java/com/example/nordic/Model/Person.java
@@ -0,0 +1,94 @@
+package com.example.nordic.Model;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String phoneNo;
+    private String email;
+    private int idAddress;
+    private String addressDetails;
+    private String city;
+    private String country;
+    private String state;
+    private String zip;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdAddress() {
+        return idAddress;
+    }
+
+    public void setIdAddress(int idAddress) {
+        this.idAddress = idAddress;
+    }
+
+    public String getAddressDetails() {
+        return addressDetails;
+    }
+
+    public void setAddressDetails(String addressDetails) {
+        this.addressDetails = addressDetails;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+}
